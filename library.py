dd14669e80caacd93de692e546023c9e2ccfc010
dhakshil <cdhakshil@gmail.com>
2026-06-09 16:04:09 +0530

    Library class structure is now created.

---
+++ library.py
@@ -0,0 +1,7 @@
+class Library:
+
+    def __init__(self, books: list, members: list, transactions: list):
+        self.books = books
+        self.members = members
+        self.transactions = transactions
+    
