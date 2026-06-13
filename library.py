430e4c54ce2cc660ad032ccbc597862a9c7ec0b4
dhakshil <cdhakshil@gmail.com>
2026-06-13 12:44:22 +0530

    List data types are now updated inside the classes.

--- library.py
+++ library.py
@@ -1,7 +1,18 @@
 class Library:
 
-    def __init__(self, books: list, members: list, transactions: list):
-        self.books = books
-        self.members = members
-        self.transactions = transactions
-    
+    def __init__(self, books = None, members = None, transactions = None):
+
+        if books is None:
+            self.books = []
+        else:
+            self.books = books
+        
+        if members is None:
+            self.members = []
+        else:
+            self.members = members
+
+        if transactions is None:
+            self.transactions = []
+        else:
+            self.transactions = transactions
